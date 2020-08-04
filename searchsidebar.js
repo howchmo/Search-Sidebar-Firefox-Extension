f0fac6bf379fe205fbc0d6ef1a430a5c48feb53e
Howard Mall <howard.mall@universalorlando.com>
2020-08-03 21:03:46 -0400

    Everything appears to work right now
    HACK: There has got a be a way for me to reveal new tabs and hide tabs without having to use a timer.

--- searchsidebar.js
+++ searchsidebar.js
@@ -12,29 +12,36 @@ browser.windows.getCurrent({populate: true}).then((windowInfo) => {
   	myWindowId = windowInfo.id;
 });
 
+function clickingSearchTerm()
+{
+	if( search_term != "" )
+		$('.search-term:contains("'+search_term+'")').removeClass("search-term-selected");
+	search_term = $(this).html();
+	$(this).addClass("search-term-selected");
+	if( isNaN(search_terms_tabs[search_term][0]) )
+	{
+		for( var i=0; i<search_terms_tabs[search_term].length; i++ )
+		{
+			browser.tabs.create({url:search_terms_tabs[search_term][i]}).then( function( tab )
+			{
+				search_terms_tabs[search_term].shift();
+			});
+		}
+	}
+	setTimeout(swapSearchTermTabs,100);
+}
+
 function checkSearchTerms()
 {
-	browser.storage.local.get("search_terms_tabs").then( function( item )
+	browser.storage.local.get("search_terms_links").then( function( item )
 	{
-		search_terms_tabs = item.search_terms_tabs;
+		search_terms_tabs = item.search_terms_links;
 		for ( var key in search_terms_tabs )
 		{
 			div=$("<div/>", {
 				class: "search-term",
 				html: key
-			}).click( function() {
-				console.log("search term selected");
-				if( search_term != "" )
-					$('.search-term:contains("'+search_term+'")').removeClass("search-term-selected");
-				search_term = $(this).html();
-				$(this).addClass("search-term-selected");
-				tabs = search_terms_tabs[search_term];
-				for( var i = 0; i<tabs.length;i++ )
-				{
-					console.log(tabs[i]);
-					browser.tabs.create({url:"https://"+tabs[i].title});
-				}
-			});
+			}).click( clickingSearchTerm );
 			$("#search-term-history").prepend(div);
 			search_terms = key;
 		}
@@ -46,31 +53,71 @@ function checkSearchTerms()
 	);
 }
 
+async function saveSearchTerms()
+{
+	console.log("saving "+JSON.stringify(search_terms_tabs));
+	var search_terms_links = {};
+	for( var key in search_terms_tabs )
+	{
+		search_terms_links[key] = [];
+		for( var i=0; i<search_terms_tabs[key].length; i++ )
+		{
+			var url = "";
+			var tabItem = search_terms_tabs[key][i]; 
+			if( isNaN(tabItem) )
+				url = tabItem;
+			else
+			{
+				var tabPromise = await browser.tabs.get(tabItem);
+				url = tabPromise.url;
+			}
+			if( key != "" )
+				search_terms_links[key].push(url);
+		}
+	}
+	browser.storage.local.set({"search_terms_links":search_terms_links});
+	console.log("DONE saving "+JSON.stringify(search_terms_links));
+}
+
 function onError(e) {
 	console.error(e);
 }
 
 function swapSearchTermTabs()
 {
-	//for( key in search_terms_tabs )
-			browser.tabs.show(search_terms_tabs[search_term]).then( function() {
-				console.log("swap "+search_term);
-				console.log("swap "+JSON.stringify(search_terms_tabs[search_term]));
-
-				var tabIdToHighlight = search_terms_tabs[search_term][0];
-				browser.tabs.get(tabIdToHighlight).then( function( info )
-				{
-					console.log(JSON.stringify(info.index));
-					tabsToHighlight = [ info.index ];
+	// if( search_terms_tabs[search_term].length > 0 )
+	{//for( key in search_terms_tabs )
+		browser.tabs.show(search_terms_tabs[search_term]).then( function()
+		{
+			console.log("swap "+search_term);
+			console.log("swap "+JSON.stringify(search_terms_tabs[search_term]));
+	
+			var tabIdToHighlight = search_terms_tabs[search_term][0];
+			browser.tabs.get(tabIdToHighlight).then( function( info )
+			{
+				console.log(JSON.stringify(info.index));
+				tabsToHighlight = [ info.index ];
 					browser.tabs.highlight({tabs:tabsToHighlight}).then( function() {
-						console.log("Hightlight "+info.index);
-						for( key in search_terms_tabs )
-							if( key != search_term )
-								browser.tabs.hide(search_terms_tabs[key]);
-					});
+					console.log("Hightlight "+info.index);
+					/*
+					for( key in search_terms_tabs )
+						if( key != search_term )
+							browser.tabs.hide(search_terms_tabs[key]);
+					*/
 				});
-
 			});
+		});
+		setTimeout( function() {
+		for( key in search_terms_tabs )
+			if( key != search_term )
+				if( search_terms_tabs[key].length > 0 )
+					if( !isNaN(search_terms_tabs[key][0]) )
+					{
+						browser.tabs.hide(search_terms_tabs[key]);
+						console.log("hide "+search_terms_tabs[key]);
+					}
+		}, 10);
+	}
 }
 
 function search()
@@ -81,16 +128,13 @@ function search()
 		div=$("<div/>", {
 			class: "search-term",
 			html: search_term_entry
-		}).click( function() {
-			if( search_term != "" )
-				$('.search-term:contains("'+search_term+'")').removeClass("search-term-selected");
-			search_term = $(this).html();
-			$(this).addClass("search-term-selected");
-			swapSearchTermTabs();
-		});
+		}).click( clickingSearchTerm );
 		$("#search-term-history").prepend(div);
 		$("#search-term-entry").html("");
+		if( search_term != "" )
+			$('.search-term:contains("'+search_term+'")').removeClass("search-term-selected");
 		search_term = search_term_entry;
+		$(this).addClass("search-term-selected");
 		browser.search.search({
 			query: search_term
 		}).then( function( searchInfo ) {
@@ -139,7 +183,7 @@ browser.tabs.onCreated.addListener((tab) => {
 				if( changeInfo.status == "complete" )
 				{
 					tab["search_term"] = search_term;
-					//browser.storage.local.set({"search_terms_tabs":search_terms_tabs});
+					saveSearchTerms();
 				}
 					// browser.tabs.onUpdated.removeListener( listener );
 			},
@@ -155,7 +199,6 @@ browser.tabs.onRemoved.addListener((tabId, removeInfo) => {
 	if( typeof search_terms_tabs !== "undefined" )
 	{
 		console.log(JSON.stringify(search_terms_tabs));
-		console.log(search_term);
 		var i = findObjectInArray(search_terms_tabs[search_term], "id",tabId);
 		if( typeof i !== "undefined" )
 		{
